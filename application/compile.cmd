0aa2421988af07f91a2204ac06bb4c877daa19af
Jeremy Kellem <60828719+kgsensei@users.noreply.github.com>
2021-03-23 07:46:02 -0400

    Create compile.cmd

---
+++ application/compile.cmd
@@ -0,0 +1,9 @@
+pyinstaller --onefile "main.py" --name "QuizzizBot" ^
+ --hiddenimport "tkinter" ^
+ --hiddenimport "selenium" ^
+ --hiddenimport "socket" ^
+ --hiddenimport "os" ^
+ --hiddenimport "time" ^
+ --hiddenimport "string" ^
+ --hiddenimport "random" ^
+ --hiddenimport "msedge"
